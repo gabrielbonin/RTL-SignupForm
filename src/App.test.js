8f7dcadf9648e48b681cb8bd34a4577684782d26
gabrielbonin <gabrielmatallo1998@gmail.com>
2024-01-04 20:08:13 -0300

    test inputs toBe

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("inputs should be initially empty", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const emailInputElement = screen.getByRole("textbox")
+  const passwordInputElement = screen.getByLabelText("Password")
+  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i)
+  expect(emailInputElement.value).toBe("")
+  expect(passwordInputElement.value).toBe("")
+  expect(confirmPasswordInputElement.value).toBe("")
+
+})
